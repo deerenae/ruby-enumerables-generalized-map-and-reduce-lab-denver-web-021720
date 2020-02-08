a8a14a17c2c297d63218526dd71215c69352f720
Denise Magner <deerenae@gmail.com>
2020-02-08 04:28:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map(array)
   new_array = []
   index = 0 
   while index < array.length do 
-    new_array.push(yeild(array[index]))
+    new_array.push(yield(array[index]))
     index += 1
   end 
   new_array
